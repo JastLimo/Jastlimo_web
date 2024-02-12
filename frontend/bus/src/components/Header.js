eb91bae1862d6f1ea42f303e131e42e8bad9f0f2
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-02-12 19:28:18 +0530

    added header

--- frontend/bus/src/components/Header.js
+++ frontend/bus/src/components/Header.js
@@ -1,22 +1,22 @@
 import React from "react";
-import "./App.css";
+import '../App.css';
 
-
-function Header() {
+const  Header = () => {
     return (
-    <header>
-        <nav>
-            <a href="#">Home</a>
-            <a href="#">Operators</a>
-            <a href="#">Products</a>
-            <a href="#">Booking</a>
-            <a href="#">Blogs</a>
-            <a href="#">Gallery </a>
-        </nav>
-    </header>
+        <div>
+            <header>
+                <nav>
+                <a href='#'>Home</a>
+                <a href='/#'>Operators</a>
+                <a href='/#'>Products</a>
+                <a href='/#'>Booking</a>
+                <a href='/#'>Blogs</a>
+                <a href='/#'>Gallery</a>
 
+                </nav>
+            </header>
+        </div>
     );
-
-};
+}
 
 export default Header;
