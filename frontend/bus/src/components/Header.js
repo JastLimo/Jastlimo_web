14ff44ac9cd324d1d84399ca62a96398851f63f2
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-02-12 13:38:18 +0530

    initial commito

---
+++ frontend/bus/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+
+function Header() {
+    return (
+    <header>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Operators</a>
+            <a href="#">Products</a>
+            <a href="#">Booking</a>
+            <a href="#">Blogs</a>
+            <a href="#">Gallery </a>
+        </nav>
+    </header>
+
+    );
+
+};
+
+export default Header;
