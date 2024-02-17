cc7ecd6212ffb0a152615574db6711ca322d4ce9
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-02-18 00:22:19 +0530

    added footer

--- frontend/bus/src/App.jsx
+++ frontend/bus/src/App.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
 import Header from './components/Header';
 import Operators from './pages/Operators';
 import Blogs from './pages/Blogs';
 import Products from './pages/Products';
 import Gallery from './pages/Gallery';
-
+import Footer from './components/Footer';
 import './App.css';
+
 
 
 function Home() {
   return (
     <div>
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
+    <div>
+        <BrowserRouter>
+          <Routes>
+        <Route path="/"  element={<Home />} />
         <Route path="/operators" element={<Operators />} />
         <Route path="/products" element={<Products />} />
         {/* <Route path="/booking" component={Booking} /> */}
@@ -29,7 +34,8 @@ function App() {
         <Route path="/gallery" element={<Gallery />} />
         {/* <Route path="book-tickets" component={BookTickets} /> */}
     </Routes>
-    </Router>
+    </BrowserRouter>
+    </div>
   );
 }
 
